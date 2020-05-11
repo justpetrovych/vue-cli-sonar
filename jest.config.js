bd03ac96af4d8a780da452f3758d54a9eea2af8c
Dmitry Ivanov <divanov@determine.com>
2020-05-11 09:27:04 +0300

    add lcov.info

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   collectCoverage: true,
+  testResultsProcessor: 'jest-sonar-reporter',
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
     '!src/main.js',
   ],
-  testResultsProcessor: 'jest-sonar-reporter',
 };
